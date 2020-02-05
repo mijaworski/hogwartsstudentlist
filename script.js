6b3ff12285d9f5b2f23c6413dfe61e37de1b3f3c
mijaworski <michjawo@gmail.com>
2020-02-06 00:12:39 +0100

    Fixed js file

--- script.js
+++ script.js
@@ -11,73 +11,33 @@ fetch("students1991.json")
     })
 
 function showStudent(student) {
-    console.log()
+    console.log(student)
     const template = document.querySelector("template").content;
     const copy = template.cloneNode(true);
     copy.querySelector(".studentname").textContent = student.fullname
-        //copy.querySelector(".studenthouse").textContent = student.house
 
     copy.querySelector("button").addEventListener("click", () => {
-        fetch("./students1991.json")
-            .then(response => response.json())
-            .then(json => console.log(student))
-            //.then(function(student) {
-            //student.forEach(showDetails)
-            //  })
-            //.then(json => details.forEach(showDetails))
-            //});
-            //});
 
-        //copy.querySelector("button").addEventListener("click", () => {
-        //fetch("./students1991.json")
-        //.then(response => response.json())
-        //.then(function(response) {
-        //   return response.json();
-        // })
-        //.then(function(student) {
-        //  student.forEach(showDetails)
-        //});
-    });
+        modal.querySelector(".modal-name").textContent = "Welcome on the page which belongs to " + student.fullname;
+        modal.querySelector(".modal-description").textContent = "This student was chosen to represent " + student.house + "'s house.";
 
-    modal.querySelector(".modal-name").textContent = "Welcome on the page which belongs to " + student.fullname;
-    modal.querySelector(".modal-description").textContent = "This student was chosen to represent " + student.house + "'s house.";
+        modal.classList.remove("hide");
 
-    modal.classList.remove("hide");
 
+    })
     document.querySelector(".studentlist").appendChild(copy);
 
+
 }
-//function showModal(uczen) {
-//console.log(uczen)
-//const template = document.querySelector("template").uczen;
-//const copy = template.cloneNode(true);
-//copy.querySelector("button").addEventListener("click", () => {
-//fetch("./students1991.json")
-// .then(response => response.json())
-//.then(function(response) {
-//    return response.json();
-//  })
-//    .then(function(uczen) {
-//          uczen.forEach(showDetails)
-//        });
-//  });
+
+//function showDetails(details) {
+//  console.log()
+
+//const modal = document.querySelector(".modal-content").content;
+//const template = document.querySelector("model-content").content;
+//const copy = modal.cloneNode(true);
+//modal.querySelector(".modal-name").textContent = "Welcome on the page which belongs to " + details.fullname;
+//modal.querySelector(".modal-description").textContent = "This student was chosen to represent " + details.house + "'s house.";
+
+//    modal.classList.remove("hide");
 //}
-
-//fetch("./students1991.json")
-//  .then(function(response) {
-//    return response.json()
-//}).then(function(data) {
-//    data.forEach(showDetails)
-//})
-
-function showDetails(details) {
-    console.log()
-
-    //const modal = document.querySelector(".modal-content").content;
-    //const template = document.querySelector("model-content").content;
-    //const copy = modal.cloneNode(true);
-    modal.querySelector(".modal-name").textContent = "Welcome on the page which belongs to " + details.fullname;
-    modal.querySelector(".modal-description").textContent = "This student was chosen to represent " + details.house + "'s house.";
-
-    modal.classList.remove("hide");
-}
