daf7e2ee41eab6c3794ab885578a94d7587e47f5
mijaworski <michjawo@gmail.com>
2020-02-05 19:03:42 +0100

    New changes

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const modal = document.querySelector(".modal-background");
+modal.addEventListener("click", () => {
+    modal.classList.add("hide");
+})
+
 fetch("./students1991.json")
     .then(function(response) {
         return response.json()
@@ -6,12 +11,40 @@ fetch("./students1991.json")
     })
 
 function showStudent(student) {
-    console.log(student)
+    console.log()
     const template = document.querySelector("template").content;
     const copy = template.cloneNode(true);
     copy.querySelector(".studentname").textContent = student.fullname
-    copy.querySelector(".studenthouse").textContent = student.house
+        //copy.querySelector(".studenthouse").textContent = student.house
+
+    copy.querySelector("button").addEventListener("click", () => {
+        fetch("./students1991.json")
+            .then(res => res.json())
+            .then(showDetails);
+    })
 
     document.querySelector(".studentlist").appendChild(copy);
 
+
+}
+
+//fetch("./students1991.json")
+//  .then(function(response) {
+//    return response.json()
+//}).then(function(data) {
+//    data.forEach(showDetails)
+//})
+
+function showDetails(students) {
+    console.log(students)
+        //copy.querySelector(".studenthouse").textContent = student.house
+
+    const modal = document.querySelector(".modal-content").content;
+    //const copy = template.cloneNode(true);
+    //modal.querySelector(".modal-name").textContent = "Welcome on the page which belongs to " + student.fullname;
+    //modal.querySelector(".modal-description").textContent = "This student was chosen to represent " + student.house + "'s house.";
+
+    //modal.classList.remove("hide");
+
+    //document.querySelector(`#student.house`).appendChild(copy);
 }
