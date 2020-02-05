9bc0723b7c13367acca8e3865f2c0f23e630e2bd
mijaworski <michjawo@gmail.com>
2020-02-06 00:43:43 +0100

    Last changes

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 const modal = document.querySelector(".modal-background");
 modal.addEventListener("click", () => {
     modal.classList.add("hide");
-})
+});
 
 fetch("students1991.json")
     .then(function(response) {
-        return response.json()
-    }).then(function(data) {
-        data.forEach(showStudent)
+        return response.json();
     })
+    .then(function(data) {
+        data.forEach(showStudent);
+    });
 
 function showStudent(student) {
-    console.log(student)
+    console.log(student);
     const template = document.querySelector("template").content;
     const copy = template.cloneNode(true);
-    copy.querySelector(".studentname").textContent = student.fullname
+    copy.querySelector(".studentname").textContent = student.fullname;
 
     copy.querySelector("button").addEventListener("click", () => {
-
-        modal.querySelector(".modal-name").textContent = "Welcome on the page which belongs to " + student.fullname;
-        modal.querySelector(".modal-description").textContent = "This student was chosen to represent " + student.house + "'s house.";
+        modal.querySelector(".modal-name").textContent =
+            "Welcome on the page which belongs to " + student.fullname;
+        modal.querySelector(".modal-description").textContent =
+            "This student was chosen to represent " + student.house + "'s house.";
 
         modal.classList.remove("hide");
-
-
-    })
+    });
     document.querySelector(".studentlist").appendChild(copy);
-
-
 }
 
 //function showDetails(details) {
